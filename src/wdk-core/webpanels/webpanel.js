62fda3d8b3d72d8d8fd81550e32268d59a82db37
TechApps <techappsv@hotmail.com>
2021-01-08 19:04:08 -0600

    0.07 beta

---
+++ src/wdk-core/webpanels/webpanel.js
@@ -0,0 +1,28 @@
+function mostrar() {
+    div = document.getElementById('flotante');
+    div.style.display = '';
+}
+function mostrar3() {
+    div = document.getElementById('flotante2');
+    div.style.display = '';
+}
+
+
+function mostrar2() {
+    div = document.getElementById('nav-body-views');
+    div.style.width = '50%';
+}
+function cerrar() {
+    div = document.getElementById('flotante');
+    div.style.display = 'none';
+}
+
+function cerrar3() {
+    div = document.getElementById('flotante2');
+    div.style.display = 'none';
+}
+
+function cerrar2() {
+    div = document.getElementById('nav-body-views');
+    div.style.width = '100%';
+}
