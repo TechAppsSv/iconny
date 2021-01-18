1b4f17fc2acb8cdd80db7e9a64d6762bb122938b
TechApps <techappsv@hotmail.com>
2021-01-18 11:52:18 -0600

    0.09 stable cupcake

--- src/wdk-core/marketforwebpanels/marketlight.js
+++ src/wdk-core/marketforwebpanels/marketlight.js
@@ -47,13 +47,13 @@ function getTasks() {
     let description = tasks[i].description;
 
     tasksView.innerHTML += `<div class="card mb-3">
-        <div class="card-body" style="background:black;border:black;">
+        <div class="card-body" >
           <p ><h4 style="text-align:center;">${title}</h4> 
           <br>
          <p style="text-align:center;"> ${description}</p>
           <br>
           <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">Eliminar</a>
-          <a href="${description}" onclick="window.open(this.href, this.target, 'width=1280,height=720'); return false;"  class="btn btn-dark ml-5">Abrir Link De ${title} NotelyApps En Ventana  </a>
+          <a href="${description}"onclick="window.open(this.href, this.target, 'width=1280,height=720'); return false;" class="btn btn-dark ml-5">Abrir Link De ${title} NotelyApps En Ventana   </a>
           </p>
         </div>
       </div>`;
