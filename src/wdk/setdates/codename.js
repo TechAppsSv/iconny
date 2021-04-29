3d14d2becf66b3749c03d3d9b20bac314ad86e9c
TechAppsSv <techappsv@hotmail.com>
2021-04-29 08:49:27 -0600

    16 Restart Compilacion 2021.29.1

--- src/wdk/setdates/codename.js
+++ src/wdk/setdates/codename.js
@@ -5,5 +5,5 @@ var text2 = document.createTextNode("Electron 12.0.4");
 document.getElementById("electron-version").appendChild(text2); 
 var text3 = document.createTextNode(" WDK: 15.1 LTS");                                    
 document.getElementById("wdk-version").appendChild(text3); 
-var text5 = document.createTextNode(" Version: 16 (Beta) Compilacion 2021.27.2");                                    
+var text5 = document.createTextNode(" Version: 16 (RC1) Compilacion 2021.29.1");                                    
 document.getElementById("version").appendChild(text5); 
