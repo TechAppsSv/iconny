5bf878f46f5a2810b9e8275da000a6afb2575822
TechAppsSv <techappsv@hotmail.com>
2021-05-06 09:19:51 -0600

    16.1 Restart Parche para correccion de bugs Compilacion 2021.06.1

--- src/wdk/setdates/codename.js
+++ src/wdk/setdates/codename.js
@@ -5,5 +5,5 @@ var text2 = document.createTextNode("Electron 12.0.4");
 document.getElementById("electron-version").appendChild(text2); 
 var text3 = document.createTextNode(" WDK: 15.1 LTS");                                    
 document.getElementById("wdk-version").appendChild(text3); 
-var text5 = document.createTextNode(" Version: 16 (Stable) Compilacion 2021.30.1");                                    
+var text5 = document.createTextNode(" Version: 16.1 (Stable) Compilacion 2021.06.1");                                    
 document.getElementById("version").appendChild(text5); 
