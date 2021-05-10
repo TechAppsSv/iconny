92cedb8c092b14b4b8f331e458b7ab98bf851c54
TechAppsSv <techappsv@hotmail.com>
2021-05-09 21:39:18 -0600

    17 Stone Alpha Compilacion 2021.05.09.03

--- src/wdk/setdates/codename.js
+++ src/wdk/setdates/codename.js
@@ -1,9 +1,9 @@
 
 var text = document.createTextNode("Stone");                                    
 document.getElementById("codename").appendChild(text); 
-var text2 = document.createTextNode("Electron 12.0.5");                                    
+var text2 = document.createTextNode("Electron 12.0.6");                                    
 document.getElementById("electron-version").appendChild(text2); 
 var text3 = document.createTextNode(" WDK: 15.2 LTS");                                    
 document.getElementById("wdk-version").appendChild(text3); 
-var text5 = document.createTextNode(" Version: 17 (Alpha) Compilacion 2021.05.06.2");                                    
+var text5 = document.createTextNode(" Version: 17 (Alpha) Compilacion 2021.05.09.3");                                    
 document.getElementById("version").appendChild(text5); 
