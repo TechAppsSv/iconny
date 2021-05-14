fcc3429727a52e476a368dce89e89aacc9dae4ee
TechAppsSv <techappsv@hotmail.com>
2021-05-14 13:19:40 -0600

    17 Stone RC1(Release Candidate 1) Compilacion 2021.05.14.2

--- src/wdk/setdates/codename.js
+++ src/wdk/setdates/codename.js
@@ -5,5 +5,5 @@ var text2 = document.createTextNode("Electron 12.0.6");
 document.getElementById("electron-version").appendChild(text2); 
 var text3 = document.createTextNode(" WDK: 15.3 LTS");                                    
 document.getElementById("wdk-version").appendChild(text3); 
-var text5 = document.createTextNode(" Version: 17 (RC1) Compilacion 2021.05.14.1");                                    
+var text5 = document.createTextNode(" Version: 17 (RC1) Compilacion 2021.05.14.2");                                    
 document.getElementById("version").appendChild(text5); 
