421885b1eff45f50ab608b9cb80ce5bd714104a7
TechAppsSv <techappsv@hotmail.com>
2021-05-21 13:26:35 -0600

    17 .1Stable Stone Compilacion 22021.5.21.1

--- src/wdk/wdk-init-webvi/index.js
+++ src/wdk/wdk-init-webvi/index.js
@@ -44,7 +44,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 1920,
     height: 1080,
-    vibrancy: 'ultra-dark',
+   
     contextIsolation: true,
 
    webPreferences: {
