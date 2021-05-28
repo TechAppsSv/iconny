2bff7c2d6268fdead8139f420417924782e27a04
TechAppsSv <techappsv@hotmail.com>
2021-05-28 13:33:42 -0600

    18 NewAge Release Candidate 1 Compilacion 2021.05.28.1

--- src/wdk/setdates/codename.js
+++ src/wdk/setdates/codename.js
@@ -1,9 +1,9 @@
 
 var text = document.createTextNode("NewAge");                                    
 document.getElementById("codename").appendChild(text); 
-var text2 = document.createTextNode("Electron 13.0.0");                                    
+var text2 = document.createTextNode("Electron 13.0.1");                                    
 document.getElementById("electron-version").appendChild(text2); 
 var text3 = document.createTextNode(" WDK: 15.5 LTS");                                    
 document.getElementById("wdk-version").appendChild(text3); 
-var text5 = document.createTextNode(" Version: 18 (Beta) Compilacion 2021.05.26.1");                                    
+var text5 = document.createTextNode(" Version: 18 (RC1 (Release Candidate)) Compilacion 2021.05.28.1");                                    
 document.getElementById("version").appendChild(text5); 
