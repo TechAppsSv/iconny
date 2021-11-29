99644cccd372916c4507cde730cef6903a3c34d1
TechApps <techappsv@hotmail.com>
2021-11-29 14:57:50 -0600

    Fix crashed tabs in Windows 11 and Windows 10, update Presentations files and update Munich Version 1 to 1.2 and update electron

--- src/munich/version/version.js
+++ src/munich/version/version.js
@@ -2,9 +2,9 @@
 
 
 
-var text2 = document.createTextNode("Electron 15.1.2 y Electron 12.0.9 (Solo MacOs)");                                    
+var text2 = document.createTextNode("Electron 15.3.2 y Electron 12.0.9 (Solo MacOs)");                                    
 document.getElementById("electron-version-munich").appendChild(text2); 
-var text3 = document.createTextNode("1 Stable");                                    
+var text3 = document.createTextNode("1.2 Stable");                                    
 document.getElementById("munich-version").appendChild(text3); 
-var text5 = document.createTextNode(" 20.1 Linux");                                    
+var text5 = document.createTextNode(" 20.3 Windows");                                    
 document.getElementById("version").appendChild(text5); 
