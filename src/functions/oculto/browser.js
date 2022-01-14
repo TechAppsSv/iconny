045091805bbbe8fdfb105ed9a2df5e85e253f67b
TechApps <techappsv@hotmail.com>
2022-01-14 14:58:33 -0600

    Iconny 21 Build 02140122

--- src/functions/oculto/browser.js
+++ src/functions/oculto/browser.js
@@ -112,21 +112,25 @@ function navigateTo(url) {
 }
 
 function doLayout() {
-  // var webview = document.querySelector('webview');
-  // var controls = document.querySelector('#controls');
-  // var controlsHeight = controls.offsetHeight;
-  // var windowWidth = document.documentElement.clientWidth;
-  // var windowHeight = document.documentElement.clientHeight;
-  // var webviewWidth = windowWidth;
-  // var webviewHeight = windowHeight - controlsHeight;
+  var webview = document.querySelector('webview');
+ var controls = document.querySelector('#controls');
+ var tab = document.querySelector('#tab-list');
+ 
+ var controlsHeight = controls.offsetHeight;
+ var tabHeight = tab.offsetHeight;
 
-  // webview.style.width = webviewWidth + 'px';
-  // webview.style.height = webviewHeight + 'px';
+ var windowWidth = document.documentElement.clientWidth;
+  var windowHeight = document.documentElement.clientHeight;
+   var webviewWidth = windowWidth;
+   var webviewHeight = windowHeight - controlsHeight - tabHeight;
 
-  // var sadWebview = document.querySelector('#sad-webview');
-  // sadWebview.style.width = webviewWidth + 'px';
-  // sadWebview.style.height = webviewHeight * 2/3 + 'px';
-  // sadWebview.style.paddingTop = webviewHeight/3 + 'px';
+ webview.style.width = webviewWidth + 'px';
+   webview.style.height = webviewHeight + 'px';
+
+  var sadWebview = document.querySelector('#sad-webview');
+   sadWebview.style.width = webviewWidth + 'px';
+   sadWebview.style.height = webviewHeight * 2/3 + 'px';
+   sadWebview.style.paddingTop = webviewHeight/3 + 'px';
 }
 
 function handleExit(event) {
--- src/munich/version/version.js
+++ src/munich/version/version.js
@@ -6,5 +6,5 @@ var text2 = document.createTextNode("Electron 16.0.5");
 document.getElementById("electron-version-munich").appendChild(text2); 
 var text3 = document.createTextNode("1.3 Stable");                                    
 document.getElementById("munich-version").appendChild(text3); 
-var text5 = document.createTextNode(" 21 (Beta 01070122)");                                    
+var text5 = document.createTextNode(" 21 (Beta 02140122)");                                    
 document.getElementById("version").appendChild(text5); 
