5458db14abea273bbdd3865cb7295b3fdc4ff52e
TechApps <techappsv@hotmail.com>
2022-01-22 15:06:55 -0600

    Iconny 22 Alpha Build 01220122

---
+++ src/functions/webapps-mananger/market.js
@@ -0,0 +1,71 @@
+document.getElementById('formTask').addEventListener('submit', saveTask);
+
+function saveTask(e) {
+  let title = document.getElementById('title').value;
+  let description = document.getElementById('description').value;
+  console.log(description)
+
+  let task = {
+    title,
+    description
+  };
+
+  if(localStorage.getItem('tasks') === null) {
+    let tasks = [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  } else {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  getTasks();
+  document.getElementById('formTask').reset();
+  e.preventDefault();
+}
+
+function deleteTask(title) {
+  console.log(title)
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  for(let i = 0; i < tasks.length; i++) {
+    if(tasks[i].title == title) {
+      tasks.splice(i, 1);
+    }
+  }
+  
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  getTasks();
+}
+
+function getTasks() {
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  let tasksView = document.getElementById('tasks');
+  tasksView.innerHTML = '';
+  for(let i = 0; i < tasks.length; i++) {
+    let title = tasks[i].title;
+    let description = tasks[i].description;
+
+    tasksView.innerHTML += `
+
+      <tr>
+
+        <td>${title}</td>
+        <td>
+        <input type="text" readonly class="form-control" style="width:40%; height:4vh;" value=" ${description}">
+
+       </td>
+        <td><a href="${description}" class="btn btn-dark" >Open link</a></td>
+ 
+        <td>
+        <a href="#" class="btn btn-danger "onclick="deleteTask('${title}')" >Delete Notely</a>
+        </td>
+
+      </tr>
+
+  
+ `;
+  }
+}
+
+getTasks();
