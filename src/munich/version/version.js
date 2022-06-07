62e57f6cc6e93d320612a92cd4d5f6d8df235c1a
TechAppsSv <jesus@MacBook-Pro-de-Jesus.local>
2022-06-07 10:29:17 -0600

    Iconny 23 Build 01070622 First Beta

--- src/munich/version/version.js
+++ src/munich/version/version.js
@@ -2,9 +2,9 @@
 
 
 
-var text2 = document.createTextNode("Electron 18.2.0");                                    
+var text2 = document.createTextNode("Electron 19.0.0");                                    
 document.getElementById("electron-version-munich").appendChild(text2); 
 var text3 = document.createTextNode("1.8 Stable");                                    
 document.getElementById("munich-version").appendChild(text3); 
-var text5 = document.createTextNode(" 23 (Alpha 01010622)");                                    
+var text5 = document.createTextNode(" 23 (Beta 01070622)");                                    
 document.getElementById("version").appendChild(text5); 
