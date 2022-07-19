934603bbb5749e7021687d0535f4eb76ccb5d57b
techapps <techappsv@hotmail.com>
2022-07-19 11:16:34 -0600

    Iconny change names version 23 stable

--- src/munich/version/version.js
+++ src/munich/version/version.js
@@ -6,5 +6,5 @@ var text2 = document.createTextNode("Electron 19.0.0");
 document.getElementById("electron-version-munich").appendChild(text2); 
 var text3 = document.createTextNode("1.8 Stable");                                    
 document.getElementById("munich-version").appendChild(text3); 
-var text5 = document.createTextNode(" 23 (Stable 01190722)");                                    
+var text5 = document.createTextNode(" 23 (Stable 02190722)");                                    
 document.getElementById("version").appendChild(text5); 
